198be8f32812b9ffc6dc8f85e3663c8af36ec535
000raihan <000raihanahmad@gmail.com>
2022-10-16 12:12:40 +0600

    updated

---
+++ src/components/Home/LiveSection.js
@@ -0,0 +1,198 @@
+import React from "react";
+import { TextInput, StyleSheet, Text, Image, Pressable } from "react-native";
+import { View } from "react-native-animatable";
+import { LinearGradient } from "expo-linear-gradient";
+import WebView from 'react-native-webview'
+
+import colors from "../../../constants/colors";
+
+const LiveSection = (props) => {
+  const onClickView = () => {
+    console.log("Im clicked")
+    props.navigation.navigate("ViewDetailsScreen")
+  }
+  // console.log("SOURCE : ", props.match_url);
+  const runFirst = `
+    var appBanners = document.getElementsByClassName('lr-imso-fix');
+    for (var i = 0; i < appBanners.length; i ++) {
+        appBanners[i].style.display = 'none';
+    }
+    var appBanners1 = document.getElementsByClassName('imso_mh__score-sum');
+    for (var i = 0; i < appBanners1.length; i ++) {
+        appBanners1[i].style.display = 'none';
+    }
+    var appBanners2 = document.getElementsByClassName('imso_mh__mh-wp');
+    for (var i = 0; i < appBanners2.length; i ++) {
+        appBanners2[i].style.display = 'none';
+    }
+    `;
+  return (
+    <View style={styles.container}>
+      <WebView
+        style={{ margin: 5, height: 280, width: 400, backgroundColor: "black" }}
+        originWhitelist={['*']}
+        // source={{ uri: props.match_url }}
+        onMessage={(event) => { }}
+        injectedJavaScript={runFirst}
+        injectedJavaScriptBeforeContentLoaded={runFirst}
+        containerStyle={{ padding: 8, backgroundColor: 'white' }}
+        source={{ uri: "https://www.google.com/search?q=t20+world+cup+2022+live+score&rlz=1C5CHFA_enBD980BD980&ei=io1LY_6dCsmcseMP-bG0IA&ved=0ahUKEwj-iubD-eP6AhVJTmwGHfkYDQQQ4dUDCA4&uact=5&oq=t20+world+cup+2022+live+score&gs_lcp=Cgdnd3Mtd2l6EAMyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgoIABBHENYEELADOgcIABCwAxBDOgwILhDIAxCwAxBDGAE6CggAELEDEIMBEEM6BwgAELEDEEM6BAgAEEM6BwgAEMkDEEM6CAgAELEDEIMBSgQIQRgASgQIRhgBUMECWJxHYIZOaAFwAXgAgAGpAYgBlAKSAQMwLjKYAQCgAQGgAQLIARHAAQHaAQYIARABGAg&sclient=gws-wiz#sie=m;/g/11rndps6wp;5;/m/026y268;dt;fp;1;;;" }}
+        scrollEnabled={false}
+      />
+      <View style={{
+        position: 'absolute',
+        marginTop: 0,
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        opacity: 0.5,
+        width: 400,
+        height: 240
+      }}>
+
+      </View>
+      {/* <View style={styles.live}>
+        <Text
+          style={{
+            width: "100%",
+            fontSize: 16,
+            color: "white",
+            overflow: "hidden",
+          }}
+        >
+          Live
+        </Text>
+      </View> */}
+      {/* <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            width: "50%",
+            marginVertical: 10,
+          }}
+        >
+          <View style={{ flex: 4 }}>
+            <Image
+              style={{ width: "100%", height: 40 }}
+              source={require("../../../assets/bd-flag.png")}
+            />
+            <Text>Bangladesh</Text>
+          </View>
+          <View style={{ flex: 3 }}>
+            <LinearGradient
+              // Button Linear Gradient
+              colors={[colors.red, "transparent"]}
+              start={[0, 1]}
+              end={[1, 0]}
+              style={{ borderBottomRightRadius: 50, borderTopRightRadius: 50 }}
+            >
+              <Text
+                style={{
+                  padding: 5,
+                  fontSize: 18,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  textAlign:"center"
+                }}
+              >
+                65/7
+              </Text>
+            </LinearGradient>
+            <Text style={{ marginHorizontal: 8, marginVertical: 4 }}>
+              20 OVR
+            </Text>
+          </View>
+        </View>
+        <Text
+          style={{ marginHorizontal: 10, fontSize: 20, fontWeight: "bold" }}
+        >
+          VS
+        </Text>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            width: "50%",
+            marginVertical: 10,
+          }}
+        >
+          <View style={{ flex: 3 }}>
+            <LinearGradient
+              // Button Linear Gradient
+              colors={[colors.red, "transparent"]}
+              start={[0, 1]}
+              end={[1, 0]}
+              style={{ borderBottomLeftRadius: 50, borderTopLeftRadius: 50 }}
+            >
+              <Text
+                style={{
+                  padding: 5,
+                  fontSize: 18,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  textAlign:"center"
+                }}
+              >
+                65/7
+              </Text>
+            </LinearGradient>
+            <Text style={{ marginHorizontal: 8, marginVertical: 4 }}>
+              20 OVR
+            </Text>
+          </View>
+          <View style={{ flex: 4 }}>
+            <Image
+              style={{ width: "100%", height: 40 }}
+              source={require("../../../assets/sr-flag.jpg")}
+            />
+            <Text style={{textAlign:"right"}}>Srilanka</Text>
+          </View>
+        </View>
+      </View> */}
+      {/* <View>
+        <Text
+          style={{
+            width: "100%",
+            fontSize:12,
+            color: colors.red,
+            overflow: "hidden",
+          }}
+        >
+         Ban WON by 3 Runs
+        </Text>
+      </View> */}
+      <Pressable onPress={() => onClickView()} style={{ paddingVertical: 5, paddingHorizontal: 10, backgroundColor: colors.primary, marginTop: 5 }}>
+        <Text style={{ color: "white" }}>View Details</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    alignSelf: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    minHeight: 280,
+    paddingVertical: 5,
+    // borderBottomWidth: 1,
+    marginVertical: 10,
+  },
+  live: {
+    backgroundColor: colors.red,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+  },
+  linearGradient: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    height: 200,
+    width: 350,
+  },
+});
+
+export default LiveSection;
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,198 +1,352 @@
-import React from "react";
-import { TextInput, StyleSheet, Text, Image, Pressable } from "react-native";
-import { View } from "react-native-animatable";
-import { LinearGradient } from "expo-linear-gradient";
-import WebView from 'react-native-webview'
-
+import React, { useEffect, useState } from "react";
+import { Portal, Provider } from "react-native-paper";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Dimensions,
+  Platform,
+  FlatList,
+  ScrollView,
+  useWindowDimensions,
+  Button,
+  Image,
+  ImageBackground,
+  Pressable, Alert,
+  BackHandler,
+  Animated
+} from "react-native";
 import colors from "../../../constants/colors";
+import { Header } from "./components/Header";
+import { Table, Row, Rows } from "react-native-table-component";
+import { WebView } from "react-native-webview";
+import MainButton from "../../components/MainButton";
+import LiveSection from "../../components/Home/LiveSection";
+import MatchButtons from "../../components/Home/MatchButtons";
+import { useDispatch, useSelector } from "react-redux";
+import * as ImagePicker from 'expo-image-picker';
 
-const LiveSection = (props) => {
-  const onClickView = () => {
-    console.log("Im clicked")
-    props.navigation.navigate("ViewDetailsScreen")
+import { CallApi } from "./api/Api";
+import { Storage } from "expo-storage";
+
+import { useIsFocused } from "@react-navigation/native";
+import * as SecureStore from "expo-secure-store";
+import PlayerFild from "../../components/Home/PlayerFild";
+
+import * as Notification from "expo-notifications";
+import * as Permission from "expo-permissions";
+
+Notification.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: true,
+      shouldShowAlert: true,
+    };
+  },
+});
+
+export const HomeScreen = (props) => {
+  const [totalPoint, setTotalPoing] = useState(null);
+  const [teamName, setTeamName] = useState(null);
+  const isFocused = useIsFocused();
+  const [select_players, setSelectedPlayers] = useState([]);
+  const [animation, setAnimation] = useState(new Animated.Value(0));
+  const [match_url, setMatch_url] = useState(null)
+  // const [animationCall, setAnimationCall] = useState(1)
+  const [pickedImagePath, setPickedImagePath] = useState(null);
+
+  // const handleAnimation = () => {
+  //   Animated.timing(animation, {
+  //     toValue: 1,
+  //     duration: 500,
+  //     useNativeDriver: true
+  //   }).start(() => {
+  //     Animated.timing(animation, {
+  //       toValue: 0,
+  //       duration: 500,
+  //       useNativeDriver: true
+  //     }).start()
+  //   })
+  // }
+  // const boxInterpolation = animation.interpolate({
+  //   inputRange: [0, 1],
+  //   outputRange: [colors.primary, colors.red]
+  // });
+
+  // const animatedStyle = {
+  //   color: boxInterpolation
+  // }
+
+  const showImagePicker = async () => {
+    // Ask the user for the permission to access the media library
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("You've refused to allow this appp to access your photos!");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync();
+
+    // Explore the result
+    // console.log(result);
+
+    if (!result.cancelled) {
+      await Storage.setItem({
+        key: "user_image",
+        value: result.uri
+      });
+      setPickedImagePath(result.uri);
+      // console.log(result.uri);
+    }
+  };
+
+
+  useEffect(() => {
+
+
+    if (isFocused) {
+      (async () => {
+        const match = await CallApi.getMatch();
+        setMatch_url(match.url)
+        // console.log("MATCH : ",match)
+
+        const userImage = await Storage.getItem({ key: 'user_image' });
+        setPickedImagePath(userImage);
+
+        const userDetailsString = await SecureStore.getItemAsync("userDetails");
+        if (userDetailsString === null) {
+          props.navigation.navigate("LoginScreen");
+        } else {
+          const userDetails = JSON.parse(userDetailsString);
+          await checkPlayerList(userDetails.id);
+        }
+      })();
+    }
+
+
+  }, [props, isFocused]);
+
+
+  useEffect(
+    () =>
+      props.navigation.addListener('beforeRemove', (e) => {
+
+        // If we don't have unsaved changes, then we don't need to do anything
+        // return;
+
+        BackHandler.exitApp();
+
+        // Prevent default behavior of leaving the screen
+        e.preventDefault();
+
+      }),
+
+    [props.navigation]
+  );
+
+  useEffect(() => {
+    Permission.getAsync(Permission.NOTIFICATIONS)
+      .then((response) => {
+        if (response.status !== "granted") {
+          return Permission.askAsync(Permission.NOTIFICATIONS);
+        }
+        return response;
+      })
+      .then((response) => {
+        if (response.status !== "granted") {
+          return;
+        }
+      });
+  }, []);
+
+  const checkPlayerList = async (user_id) => {
+    CallApi.player_list(user_id).then(async (result) => {
+      // console.log(result.result, "===========")
+      if (result.success) {
+        if (result.result.length === 0) {
+          props.navigation.navigate("CreateTeamStackScreen");
+
+        } else {
+          await Storage.setItem({
+            key: "select_player",
+            value: JSON.stringify({ team_name: result.result[0].team_name })
+          });
+          setTotalPoing(result.total_point);
+          setTeamName(result.result[0].team_name);
+          setSelectedPlayers(result.result);
+        }
+      } else {
+        Alert.alert('Error', result.message, [
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ]);
+        // console.log("error", result.error);
+      }
+    }, (error) => {
+      console.log("=====", error)
+      alert("Invalid data.");
+    }
+    );
   }
-  // console.log("SOURCE : ", props.match_url);
-  const runFirst = `
-    var appBanners = document.getElementsByClassName('lr-imso-fix');
-    for (var i = 0; i < appBanners.length; i ++) {
-        appBanners[i].style.display = 'none';
-    }
-    var appBanners1 = document.getElementsByClassName('imso_mh__score-sum');
-    for (var i = 0; i < appBanners1.length; i ++) {
-        appBanners1[i].style.display = 'none';
-    }
-    var appBanners2 = document.getElementsByClassName('imso_mh__mh-wp');
-    for (var i = 0; i < appBanners2.length; i ++) {
-        appBanners2[i].style.display = 'none';
-    }
-    `;
-  return (
-    <View style={styles.container}>
-      <WebView
-        style={{ margin: 5, height: 280, width: 400, backgroundColor: "black" }}
-        originWhitelist={['*']}
-        // source={{ uri: props.match_url }}
-        onMessage={(event) => { }}
-        injectedJavaScript={runFirst}
-        injectedJavaScriptBeforeContentLoaded={runFirst}
-        containerStyle={{ padding: 8, backgroundColor: 'white' }}
-        source={{ uri: "https://www.google.com/search?q=t20+world+cup+2022+live+score&rlz=1C5CHFA_enBD980BD980&ei=io1LY_6dCsmcseMP-bG0IA&ved=0ahUKEwj-iubD-eP6AhVJTmwGHfkYDQQQ4dUDCA4&uact=5&oq=t20+world+cup+2022+live+score&gs_lcp=Cgdnd3Mtd2l6EAMyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgoIABBHENYEELADOgcIABCwAxBDOgwILhDIAxCwAxBDGAE6CggAELEDEIMBEEM6BwgAELEDEEM6BAgAEEM6BwgAEMkDEEM6CAgAELEDEIMBSgQIQRgASgQIRhgBUMECWJxHYIZOaAFwAXgAgAGpAYgBlAKSAQMwLjKYAQCgAQGgAQLIARHAAQHaAQYIARABGAg&sclient=gws-wiz#sie=m;/g/11rndps6wp;5;/m/026y268;dt;fp;1;;;" }}
-        scrollEnabled={false}
-      />
-      <View style={{
-        position: 'absolute',
-        marginTop: 0,
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        opacity: 0.5,
-        width: 400,
-        height: 240
-      }}>
 
+  // useEffect(() => {
+
+  // }, [])
+
+  // const getMatch = async ()=>{
+  //   const match = await CallApi.getMatch();
+  //   console.log(match)
+  // }
+
+
+
+  const onPress = async () => {
+    await Storage.setItem({
+      key: "select_player_list",
+      value: JSON.stringify([])
+    });
+    // props.navigation.navigate("MyTeamScreen", {isEdit: true})
+    props.navigation.navigate("EditTeamScreen", { isEdit: true })
+  }
+
+
+
+  return (
+    <Provider>
+      <View style={styles.container}>
+        <Header navigation={props.navigation} />
+        {/* <View style={{backgroundColor:colors.primary}}> */}
+        <View style={styles.pointBg}>
+            <View
+              style={{
+                width: "100%",
+                // height: "80%",
+                backgroundColor: "white",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "row",
+              }}
+            >
+              <View
+                style={{
+                  flex: 4,
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  paddingHorizontal: 10,
+                  paddingVertical: 18,
+                }}
+              >
+                <View
+                  style={{
+                    height: 50,
+                    width: 50,
+                    borderRadius: 50 / 2,
+                    overflow: "hidden",
+                    marginRight: 8,
+                  }}
+                >
+                  <Pressable onPress={() => showImagePicker() }>
+                    {
+                      pickedImagePath === null ?
+                          <Image
+                              resizeMode="cover"
+                              style={{ height: "100%", width: "100%" }}
+                              source={require("../../../assets/team_logo.png")}
+                          />
+                          :
+                          <Image
+                              resizeMode="cover"
+                              style={{ height: "100%", width: "100%" }}
+                              source={{uri: pickedImagePath}}
+                          />
+                    }
+                  </Pressable>
+                </View>
+
+                <View>
+                  <Text style={{ fontSize: 12, color: colors.red }}>
+                    Team Name
+                  </Text>
+                  <Text style={styles.pointName}>{teamName && teamName}</Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  flex: 3,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  // height: "100%",
+                }}
+              >
+                <Text style={styles.pointStyle}>
+                  {(totalPoint && totalPoint[0].point) || 0}
+                </Text>
+                {/* <Text
+                  style={{
+
+                  }}
+                >
+
+                </Text> */}
+              </View>
+              {/* </View> */}
+            </View>
+          </View>
+        <LiveSection match_url={match_url} navigation={props.navigation} />
+        <ScrollView>
+
+
+          <MatchButtons navigation={props.navigation} />
+
+          <View style={{ width: "90%", alignSelf: "center" }}>
+            <Image
+              style={{ width: "100%", resizeMode: "contain" }}
+              source={require("../../../assets/brand_gif.gif")}
+            />
+          </View>
+
+          <PlayerFild players={select_players} onPress={onPress} />
+        </ScrollView>
       </View>
-      {/* <View style={styles.live}>
-        <Text
-          style={{
-            width: "100%",
-            fontSize: 16,
-            color: "white",
-            overflow: "hidden",
-          }}
-        >
-          Live
-        </Text>
-      </View> */}
-      {/* <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            width: "50%",
-            marginVertical: 10,
-          }}
-        >
-          <View style={{ flex: 4 }}>
-            <Image
-              style={{ width: "100%", height: 40 }}
-              source={require("../../../assets/bd-flag.png")}
-            />
-            <Text>Bangladesh</Text>
-          </View>
-          <View style={{ flex: 3 }}>
-            <LinearGradient
-              // Button Linear Gradient
-              colors={[colors.red, "transparent"]}
-              start={[0, 1]}
-              end={[1, 0]}
-              style={{ borderBottomRightRadius: 50, borderTopRightRadius: 50 }}
-            >
-              <Text
-                style={{
-                  padding: 5,
-                  fontSize: 18,
-                  fontWeight: "bold",
-                  color: colors.primary,
-                  textAlign:"center"
-                }}
-              >
-                65/7
-              </Text>
-            </LinearGradient>
-            <Text style={{ marginHorizontal: 8, marginVertical: 4 }}>
-              20 OVR
-            </Text>
-          </View>
-        </View>
-        <Text
-          style={{ marginHorizontal: 10, fontSize: 20, fontWeight: "bold" }}
-        >
-          VS
-        </Text>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            width: "50%",
-            marginVertical: 10,
-          }}
-        >
-          <View style={{ flex: 3 }}>
-            <LinearGradient
-              // Button Linear Gradient
-              colors={[colors.red, "transparent"]}
-              start={[0, 1]}
-              end={[1, 0]}
-              style={{ borderBottomLeftRadius: 50, borderTopLeftRadius: 50 }}
-            >
-              <Text
-                style={{
-                  padding: 5,
-                  fontSize: 18,
-                  fontWeight: "bold",
-                  color: colors.primary,
-                  textAlign:"center"
-                }}
-              >
-                65/7
-              </Text>
-            </LinearGradient>
-            <Text style={{ marginHorizontal: 8, marginVertical: 4 }}>
-              20 OVR
-            </Text>
-          </View>
-          <View style={{ flex: 4 }}>
-            <Image
-              style={{ width: "100%", height: 40 }}
-              source={require("../../../assets/sr-flag.jpg")}
-            />
-            <Text style={{textAlign:"right"}}>Srilanka</Text>
-          </View>
-        </View>
-      </View> */}
-      {/* <View>
-        <Text
-          style={{
-            width: "100%",
-            fontSize:12,
-            color: colors.red,
-            overflow: "hidden",
-          }}
-        >
-         Ban WON by 3 Runs
-        </Text>
-      </View> */}
-      <Pressable onPress={() => onClickView()} style={{ paddingVertical: 5, paddingHorizontal: 10, backgroundColor: colors.primary, marginTop: 5 }}>
-        <Text style={{ color: "white" }}>View Details</Text>
-      </Pressable>
-    </View>
+    </Provider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+  pointBg: {
     width: "100%",
-    alignSelf: "center",
+    // height: 180,
+    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white",
-    minHeight: 280,
-    paddingVertical: 5,
-    // borderBottomWidth: 1,
-    marginVertical: 10,
+    marginTop: 10,
   },
-  live: {
-    backgroundColor: colors.red,
-    paddingHorizontal: 10,
-    paddingVertical: 4,
+  pointTextSec: {},
+  pointName: {
+    color: colors.primary,
+    fontSize: 20,
+    fontWeight: "bold",
   },
-  linearGradient: {
-    alignItems: "center",
+  highlightMatch: {
+    marginTop: 10,
+    alignSelf: "center",
+    backgroundColor: "black",
+    width: "90%",
+    height: 150,
     justifyContent: "center",
-    borderRadius: 5,
-    height: 200,
-    width: 350,
+    alignItems: "center",
   },
-});
+  pointStyle: {
+    color: colors.primary,
+    fontSize: 35,
+    fontWeight: "bold",
+    // margin: -10,
 
-export default LiveSection;
+  },
+
+});
